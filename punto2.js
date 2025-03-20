6efd9e97e5d21680b221ffea87e2a4c5497638a9
JohanDavidGarces <johan.davidg@comunidad.iush.edu.co>
2025-03-20 01:04:42 -0500

    Eggster code point 2

--- punto2.js
+++ punto2.js
@@ -39,9 +39,35 @@ generar_numero(): Función para generar un número aleatorio.
 
 adivinar_numero(): Función para gestionar el juego de adivinanza. 
 */
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')() //Para usar prompt
+const listGames = [] //lista donde pondré las victorias y derrotas
 function verificar_numero(numero_verificar) {
     
+}
+function juego(){ 
+    let tryAgainFlag=true; //bandera general para repetir el juego
+    while(tryAgainFlag){
+        adivinar_numero(generar_numero()) //llama las 2 funciones, el generar_numero y lo envia a la función adivinar_numero
+        let playAgainYNflag=true; // bandera para verificar que el valor dado sea y ó n
+        while(playAgainYNflag){ 
+        let playAgain=prompt("¿Deseas jugar de nuevo? (y/n): ") //pide si quiere jugar de nuevo
+        if (playAgain=="y") {
+
+            playAgainYNflag = false    
+        }else if (playAgain=="n"){
+            
+            playAgainYNflag = false    
+            return; //saca del juego en caso de que no quiera volver a jugar
+        }else{ // en caso de que no sea y ó n
+            console.log("Caracter no valido, intenta de nuevo")
+        }
+        }
+    }
+}
+function contadorVictoriasDerrotas(){
+    let Derrotas = listGames.filter(Defeat => Defeat=="D") //crea un array de derrotas
+    let Victorias = listGames.filter(Victory=> Victory=="V") //crea un array de victorias
+    console.log(`Derrotas totales: ${Derrotas.length} \nVictorias totales: ${Victorias.length}`) //las victorias son la cantidad de V y las derrotas la cantidad de D
 }
 function generar_numero() {
     return Math.floor(Math.random() * (99) + 1) //genera un número aleatorio del 1 al 100 con la formula: (maxNumber - minNumber) + minNumber)
@@ -51,10 +77,23 @@ function adivinar_numero(realNumber) {
     let attempts=0; //contador de intentos
     while (flag) { //El ciclo principal de la función
         attempts++; // suma los intentos
-        let userNumber=prompt("ingresar x número") //ingresa los números en userNumber
+        let userNumber=prompt("ingresar un número de 1 a 100 ó 0 para rendirte ") //ingresa los números en userNumber
+        if (userNumber==0){
+            break; //en caso de que se rinda con 0
+        }
         userNumber==realNumber?flag=false:0; // verifica si adivinó el número
         flag==true?(userNumber<realNumber?console.log(`${userNumber} es más pequeño que el número aleatorio`):console.log(`${userNumber} es más grande que el número aleatorio`)):0; //verifica si ya se adivinó y luego determina si el número es mayor o menor
     }
-    console.log(`¡Felicidades!, has logrado encontrar el número ${realNumber} después de ${attempts} intento/s`) //felicita por encontrar el número y muestra la cantidad de intentos 
+    if(flag){ //si se rinde la bandera seguirá siendo verdadero
+    (console.log(`¡Lastima!, el número era ${realNumber} intentos: ${attempts}`)) 
+    listGames.push("D")
+    }
+    else{ //si no se rinde y gana:
+    (console.log(`¡Felicidades!, has logrado encontrar el número ${realNumber} después de ${attempts} intento/s`)) //muestra el numero si se rinde o felicita por encontrar el número y muestra la cantidad de intentos
+    listGames.push("V")
+    }
 }
-adivinar_numero(generar_numero()) //llama las 2 funciones, el generar_numero y lo envia a la función adivinar_numero
+console.log("Bienvenido ") 
+juego()
+console.log("Gracias por jugar")
+contadorVictoriasDerrotas()
