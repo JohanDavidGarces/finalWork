67a5d7b045680640930927ed1ac8092a75727540
JohanDavidGarces <johan.davidg@comunidad.iush.edu.co>
2025-03-20 00:01:47 -0500

    main code point 2

--- punto2.js
+++ punto2.js
@@ -39,10 +39,22 @@ generar_numero(): Función para generar un número aleatorio.
 
 adivinar_numero(): Función para gestionar el juego de adivinanza. 
 */
+const prompt = require('prompt-sync')()
+function verificar_numero(numero_verificar) {
+    
+}
 function generar_numero() {
     return Math.floor(Math.random() * (99) + 1) //genera un número aleatorio del 1 al 100 con la formula: (maxNumber - minNumber) + minNumber)
 }
 function adivinar_numero(realNumber) {
-
+    flag=true //una bandera para controlar el ciclo
+    let attempts=0; //contador de intentos
+    while (flag) { //El ciclo principal de la función
+        attempts++; // suma los intentos
+        let userNumber=prompt("ingresar x número") //ingresa los números en userNumber
+        userNumber==realNumber?flag=false:0; // verifica si adivinó el número
+        flag==true?(userNumber<realNumber?console.log(`${userNumber} es más pequeño que el número aleatorio`):console.log(`${userNumber} es más grande que el número aleatorio`)):0; //verifica si ya se adivinó y luego determina si el número es mayor o menor
+    }
+    console.log(`¡Felicidades!, has logrado encontrar el número ${realNumber} después de ${attempts} intento/s`) //felicita por encontrar el número y muestra la cantidad de intentos 
 }
-adivinar_numero(generar_numero())
+adivinar_numero(generar_numero()) //llama las 2 funciones, el generar_numero y lo envia a la función adivinar_numero
